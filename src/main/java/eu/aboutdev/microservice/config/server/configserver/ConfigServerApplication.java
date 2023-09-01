97f421f18fe12d3830eaa013b41e31c1fed45ef2
Dominik Hahn <develop.illuminati@gmail.com>
2023-09-01 22:08:41 +0200

    fix yaml

--- src/main/java/eu/aboutdev/microservice/config/server/configserver/ConfigServerApplication.java
+++ src/main/java/eu/aboutdev/microservice/config/server/configserver/ConfigServerApplication.java
@@ -5,11 +5,13 @@ import io.micrometer.core.instrument.Metrics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
